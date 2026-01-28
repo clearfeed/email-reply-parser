f60df68c163ef29c967e437c33a8f64ac0edff3b
achitJ <ajain2801@gmail.com>
2026-01-28 14:42:57 +0530

    add new feature

--- lib/emailreplyparser.ts
+++ lib/emailreplyparser.ts
@@ -11,6 +11,7 @@
 
 // PROJECT: LIB
 import EmailParser from "./parser/emailparser.js";
+import RegexList from "./regex.js";
 
 /**
  * EmailReplyParser
@@ -35,6 +36,38 @@ class EmailReplyParser {
    */
   public parseReplied(text: string) {
     return this.read(text).getQuotedText();
+  }
+
+  /**
+   * Add multiple custom quote header regexes
+   */
+  public addQuoteHeaderRegexes(regexes: RegExp[]): this {
+    RegexList.addQuoteHeaderRegexes(regexes);
+    return this;
+  }
+
+  /**
+   * Add multiple custom signature regexes
+   */
+  public addSignatureRegexes(regexes: RegExp[]): this {
+    RegexList.addSignatureRegexes(regexes);
+    return this;
+  }
+
+  /**
+   * Reset quote header regexes to default
+   */
+  public resetQuoteHeaderRegexes(): this {
+    RegexList.resetQuoteHeaderRegexes();
+    return this;
+  }
+
+  /**
+   * Reset signature regexes to default
+   */
+  public resetSignatureRegexes(): this {
+    RegexList.resetSignatureRegexes();
+    return this;
   }
 }
 
